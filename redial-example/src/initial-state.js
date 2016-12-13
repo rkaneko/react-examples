ad2d8cbeb86b8fe3dec4335d5f376218eba8c7a0
rkaneko <kanek.o.dev@gmail.com>
2016-12-14 01:23:24 +0900

    `add` initial state

---
+++ redial-example/src/initial-state.js
@@ -0,0 +1,14 @@
+const initialState = {
+  entries: [
+    {
+      title: 'Blahblah'
+      , body: 'This is the blahblah.'
+    }
+    , {
+      title: 'FooBar'
+      , body: 'This is the foobar.'
+    }
+  ]
+};
+
+export default initialState;
