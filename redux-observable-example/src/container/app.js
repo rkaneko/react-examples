87e557b04d810fd389cec01ba31f215a76adfa27
rkaneko <kanek.o.dev@gmail.com>
2017-01-04 22:56:28 +0900

    `add` a minimal redux example

--- redux-observable-example/src/container/app.js
+++ redux-observable-example/src/container/app.js
@@ -1,18 +1,27 @@
 'use strict';
 
 const React = require('react');
-const { Component } = React;
+const {Component} = React;
+const {connect} = require('react-redux');
 
 const Header = require('../component/header.js');
 
 class App extends Component {
   render() {
+    const {name} = this.props;
     return (
       <div>
+        {name}
         <Header />        
       </div>
     );
   }
 }
 
-module.exports = App;
+function mapStateToProps(state) {
+  return state;
+}
+
+const ConnectedApp = connect(mapStateToProps)(App);
+
+module.exports = ConnectedApp;
--- redux-observable-example/src/js/index.js
+++ redux-observable-example/src/js/index.js
@@ -2,10 +2,29 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const {Provider} = require('react-redux');
+const {createStore} = require('redux');
 
 const App = require('../container/app.js');
 
+const initialState = {
+  name: 'React'
+};
+
+function reducer(state = initialState, action) {
+  const {type, paylaod} = action;
+  switch (type) {
+  default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+
 ReactDOM.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   , document.getElementsByClassName('main-container')[0]
 );
